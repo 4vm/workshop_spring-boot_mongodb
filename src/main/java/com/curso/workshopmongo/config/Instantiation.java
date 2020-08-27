522b0daafb2e8c441a4b743df420594592845930
4vm <amadeumrzr@gmail.com>
2020-08-27 14:35:54 -0300

    Referenciando os posts do usuario

--- src/main/java/com/curso/workshopmongo/config/Instantiation.java
+++ src/main/java/com/curso/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.curso.workshopmongo.domain.Post;
 import com.curso.workshopmongo.domain.User;
+import com.curso.workshopmongo.dto.AuthorDTO;
 import com.curso.workshopmongo.repository.PostRepository;
 import com.curso.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,14 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz", maria);
-
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
